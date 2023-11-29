3eec6d34808bd4c644ef385745a1a153b3fd4afc
mderler <derler190063@sr.htlweiz.at>
2023-11-29 10:45:25 +0100

    Setup queues

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,51 @@ QueueHandle_t dataQueue;
 
 void readSensor(void *parameters)
 {
+    int sensorData;
     for (;;)
     {
         vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+        // read sensor here and store in sensorData variable
+
+        if (xQueueSend(dataQueue, &sensorData, 0) == pdPASS)
+        {
+            Serial.print("Sent sensor data to queue: ");
+            Serial.println(sensorData);
+        }
+        else
+        {
+            Serial.println("Error on sending to queue");
+        }
+    }
+}
+
+void sendData(void *parameters)
+{
+    int sensorData;
+    for (;;)
+    {
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+
+        if (xQueueReceive(dataQueue, &sensorData, portMAX_DELAY) == pdTRUE)
+        {
+            // send sensorData to consumer
+        }
+        else
+        {
+            Serial.println("Error on reading queue");
+        }
     }
 }
 
 void setup()
 {
+    Serial.begin(9600);
+
     dataQueue = xQueueCreate(QUEUE_SIZE, sizeof(int));
 
     xTaskCreate(readSensor, "Read Sensor", 1000, NULL, 1, NULL);
+    xTaskCreate(sendData, "Send Data", 1000, NULL, 1, NULL);
 }
 
 void loop()
